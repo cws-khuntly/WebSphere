1f46a3e8715ae09e91847b331693de5b01da3fc5
Kevin Huntly <cws-khuntly@users.noreply.github.com>
2024-02-16 08:21:43 -0500

    Update gpgutils.sh

--- lib/dotfiles/gpgutils.sh
+++ lib/dotfiles/gpgutils.sh
@@ -15,6 +15,7 @@ function generateGpgKeys()
     set +o noclobber;
     cname="sshutils.sh";
     function_name="${cname}#${FUNCNAME[0]}";
+    ret_code=0;
     return_code=0;
     error_count=0;
 
@@ -55,67 +56,6 @@ function generateGpgKeys()
         fi
     fi
 
-    for AVAILABLE_SSH_KEY_TYPE in ${SSH_KEY_TYPES[@]}; do
-        if [[ -n "${ENABLE_DEBUG}" ]] && [[ "${ENABLE_DEBUG}" == "${_TRUE}" ]] && [[ "${LOGGING_LOADED}" == "${_TRUE}" ]]; then
-            writeLogEntry "DEBUG" "${cname}" "${function_name}" "${LINENO}" "AVAILABLE_SSH_KEY_TYPE -> ${AVAILABLE_SSH_KEY_TYPE}";
-        fi
-
-        SSH_KEY_TYPE="$(cut -d "," -f 1 <<< "${AVAILABLE_SSH_KEY_TYPE}")";
-        SSH_KEY_SIZE="$(cut -d "," -f 2 <<< "${AVAILABLE_SSH_KEY_TYPE}")";
-        SSH_KEY_FILENAME="id_${SSH_KEY_TYPE}";
-
-        if [[ -n "${ENABLE_DEBUG}" ]] && [[ "${ENABLE_DEBUG}" == "${_TRUE}" ]] && [[ "${LOGGING_LOADED}" == "${_TRUE}" ]]; then
-            writeLogEntry "DEBUG" "${cname}" "${function_name}" "${LINENO}" "SSH_KEY_TYPE -> ${SSH_KEY_TYPE}";
-            writeLogEntry "DEBUG" "${cname}" "${function_name}" "${LINENO}" "SSH_KEY_SIZE -> ${SSH_KEY_SIZE}";
-        fi
-
-        ## if it doesnt exist then make it. if it does exist skip it
-        if [[ ! -f "${HOME}"/.ssh/"${SSH_KEY_FILENAME}" ]]; then
-            ssh-keygen -b "${SSH_KEY_SIZE}" -t "${SSH_KEY_TYPE}" -f "${TMPDIR:-${USABLE_TMP_DIR}}/${SSH_KEY_FILENAME}";
-            ret_code="${?}";
-
-            if [[ -n "${ENABLE_DEBUG}" ]] && [[ "${ENABLE_DEBUG}" == "${_TRUE}" ]] && [[ "${LOGGING_LOADED}" == "${_TRUE}" ]]; then
-                writeLogEntry "DEBUG" "${cname}" "${function_name}" "${LINENO}" "ret_code -> ${ret_code}";
-            fi
-
-            if [[ -z "${ret_code}" ]] || (( ret_code != 0 )); then
-                (( error_count += 1 ));
-
-                writeLogEntry "ERROR" "${cname}" "${function_name}" "${LINENO}" "SSH keyfile generation for type ${SSH_KEY_TYPE} failed with return code ${ret_code}";
-            else
-                if [[ ! -f "${TMPDIR:-${USABLE_TMP_DIR}}/${SSH_KEY_TYPE}" ]]; then
-                    (( error_count += 1 ));
-
-                    writeLogEntry "ERROR" "${cname}" "${function_name}" "${LINENO}" "SSH keyfile generation for type ${SSH_KEY_TYPE} failed with return code ${ret_code}";
-                else
-                    if [[ -n "${ENABLE_DEBUG}" ]] && [[ "${ENABLE_DEBUG}" == "${_TRUE}" ]] && [[ "${LOGGING_LOADED}" == "${_TRUE}" ]]; then
-                        writeLogEntry "DEBUG" "${cname}" "${function_name}" "${LINENO}" "EXEC: mv ${TMPDIR:-${USABLE_TMP_DIR}}/${SSH_KEY_FILENAME} ${HOME}/.ssh/${SSH_KEY_FILENAME}";
-                        writeLogEntry "DEBUG" "${cname}" "${function_name}" "${LINENO}" "EXEC: mv ${TMPDIR:-${USABLE_TMP_DIR}}/${SSH_KEY_FILENAME}.pub ${HOME}/.ssh/${SSH_KEY_FILENAME}.pub";
-                    fi
-
-                    ## relocate the keyfiles to the user home directory
-                    mv "${TMPDIR:-${USABLE_TMP_DIR}}/${SSH_KEY_FILENAME}" "${HOME}"/.ssh/"${SSH_KEY_FILENAME}";
-                    mv "${TMPDIR:-${USABLE_TMP_DIR}}/${SSH_KEY_FILENAME}.pub" "${HOME}"/.ssh/"${SSH_KEY_FILENAME}.pub";
-
-                    ## make sure they exist
-                    if [[ ! -f "${HOME}"/.ssh/"${SSH_KEY_FILENAME}" ]] || [[ ! -f "${HOME}"/.ssh/"${SSH_KEY_FILENAME}.pub" ]]; then
-                        (( error_count += 1 ));
-
-                        writeLogEntry "ERROR" "${cname}" "${function_name}" "${LINENO}" "SSH keyfile generation for type ${SSH_KEY_TYPE} failed with return code ${ret_code}";
-                    fi
-                fi
-            fi
-        fi
-
-        [[ -n "${SSH_KEY_TYPE}" ]] && unset -v SSH_KEY_TYPE;
-        [[ -n "${SSH_KEY_SIZE}" ]] && unset -v SSH_KEY_SIZE;
-        [[ -n "${SSH_KEY_FILENAME}" ]] && unset -v SSH_KEY_FILENAME;
-    done
-        
-    [[ -n "${SSH_KEY_TYPE}" ]] && unset -v SSH_KEY_TYPE;
-    [[ -n "${SSH_KEY_SIZE}" ]] && unset -v SSH_KEY_SIZE;
-    [[ -n "${SSH_KEY_FILENAME}" ]] && unset -v SSH_KEY_FILENAME;
-    [[ -n "${AVAILABLE_SSH_KEY_TYPE}" ]] && unset -v AVAILABLE_SSH_KEY_TYPE;
     [[ -n "${ret_code}" ]] && unset -v ret_code;
 
     if [[ -n "${error_count}" ]] && (( error_count != 0 )); then return_code="${error_count}"; fi
