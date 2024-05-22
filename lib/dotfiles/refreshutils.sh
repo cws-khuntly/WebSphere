729d2e12fab414ea482f1387d4fec56b6c450e25
Kevin Huntly <cws-khuntly@users.noreply.github.com>
2024-05-22 08:27:46 -0400

    Update refreshutils.sh

--- lib/dotfiles/refreshutils.sh
+++ lib/dotfiles/refreshutils.sh
@@ -90,7 +90,7 @@ function refreshFiles()
 
                 if [[ -z "${ret_code}" ]] || (( ret_code != 0 )); then
                     [[ "${LOGGING_LOADED}" == "${_TRUE}" ]] && writeLogEntry "FILE" "ERROR" "${$}" "${cname}" "${LINENO}" "${function_name}" "An error occurred checking host availability for host ${refresh_host}. Please review logs.";
-                else
+                fi
             fi
 
             if (( ${#returnedHostInfo[*]} != 0 )) && (( ret_code == 0 )) || [[ -n "${force_exec}" ] && [ "${force_exec}" == "${_TRUE}" ]]; then
