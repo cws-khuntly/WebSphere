86d910dc45791b8b90ec21c4d1d6171867b21229
Kevin Huntly <cws-khuntly@users.noreply.github.com>
2024-05-28 12:25:55 -0400

    Update installutils.sh

--- lib/dotfiles/installutils.sh
+++ lib/dotfiles/installutils.sh
@@ -77,7 +77,7 @@ function installFiles()
                 [[ -n "${function_name}" ]] && unset -v function_name;
                 [[ -n "${ret_code}" ]] && unset -v ret_code;
 
-                returnedHostInfo=( "$(validateHostAddress "${target_host}" "${target_port}")" );
+                returnedHostInfo="$(validateHostAddress "${target_host}" "${target_port}")";
                 ret_code="${?}";
 
                 cname="installutils.sh";
