b1d097e46cd1a9db86eab98746a9aba231368948
Kevin Huntly <kmhuntly@gmail.com>
2025-02-13 20:37:08 -0500

    remove tmux

---
+++ lib/wasadmin/syncNodes.py
@@ -0,0 +1,48 @@
+#==============================================================================
+#
+#          FILE:  configureDMGR.py
+#         USAGE:  wsadmin.sh -lang jython -f configureDMGR.py
+#     ARGUMENTS:  wasVersion
+#   DESCRIPTION:  Executes an scp connection to a pre-defined server
+#
+#       OPTIONS:  ---
+#  REQUIREMENTS:  ---
+#          BUGS:  ---
+#         NOTES:  ---
+#        AUTHOR:  Kevin Huntly <kmhuntly@gmail.com>
+#       COMPANY:  ---
+#       VERSION:  1.0
+#       CREATED:  ---
+#      REVISION:  ---
+#==============================================================================
+
+import sys
+import time
+import logging
+
+global logger = logging.getlog(__name__)
+global lineSplit = java.lang.System.getProperty("line.separator")
+
+global targetCell = AdminControl.getCell()
+global nodeList = AdminTask.listManagedNodes().split(lineSplit)
+
+def performNodeSync:
+    print("Synchronizing all nodes...")
+
+    for node in nodeList:
+        nodeRepo = AdminControl.completeObjectName('type=ConfigRepository,process=nodeagent,node=' + node + ',*')
+
+        if nodeRepo:
+            AdminControl.invoke(nodeRepo, 'refreshRepositoryEpoch')
+
+        syncNode = AdminControl.completeObjectName('cell=' + targetCell + ',node=' + node + ',type=NodeSync,*')
+
+        if syncNode:
+            AdminControl.invoke(syncNode, 'sync')
+
+        continue
+
+##################################
+# main
+#################################
+performNodeSync()
