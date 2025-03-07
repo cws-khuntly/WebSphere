236ed37ce07cf708f7597a077909947b1b606244
Kevin Huntly <kmhuntly@gmail.com>
2025-03-06 19:02:46 -0500

    more changes, stopping for tonight

--- AppServer/wsadmin/includes/includes.py
+++ AppServer/wsadmin/includes/includes.py
@@ -17,113 +17,245 @@
 #==============================================================================
 
 import os
+import logging
 import zipfile
 
+configureLogging(str("/home/wasadm/workspace/WebSphere/AppServer/wsadmin/config/logging.properties"))
+errorLogger = logging.getLogger(str("error-logger"))
+debugLogger = logging.getLogger(str("debug-logger"))
+infoLogger = logging.getLogger(str("info-logger"))
+
 def getAppDisplayName(filePath):
+    debugLogger.log(logging.DEBUG, str("ENTER: includes#getAppDisplayName(filePath)"))
+    debugLogger.log(logging.DEBUG, str(filePath))
+
     if (os.path.exists(filePath)):
-        start_string = "<display-name>"
-        end_string = "</display-name>"
+        startPoint = "<display-name>"
+        endPoint = "</display-name>"
+
+        debugLogger.log(logging.DEBUG, str(startPoint))
+        debugLogger.log(logging.DEBUG, str(endPoint))
 
         if (filePath.endswith(".war")) or (filePath.endswith(".ear")):
-            zf = zipfile.ZipFile(filePath, "r")
-            nl = zf.namelist()
+            zipFile = zipfile.ZipFile(filePath, "r")
+            nameListing = zipFile.namelist()
 
-            for name in nl:
-                if (name.lower().find(("application.xml")) != -1):
-                    appxml = zf.read(name)
-                    start_index = appxml.find(start_string) + len(start_string)
-                    end_index = appxml.find(end_string)
+            debugLogger.log(logging.DEBUG, str(zipFile))
+            debugLogger.log(logging.DEBUG, str(nameListing))
 
-                    appDisplayName = appxml[start_index:end_index]
-                elif (name.lower().find(("web.xml")) != -1):
-                    webxml = zf.read(name)
-                    start_index = webxml.find(start_string) + len(start_string)
-                    end_index = webxml.find(end_string)
+            for nameEntry in (nameListing):
+                debugLogger.log(logging.DEBUG, str(nameEntry))
 
-                    appDisplayName = webxml[start_index:end_index]
+                if (filePath.endswith(".ear")):
+                    if (nameEntry.lower().find(("application.xml")) != -1):
+                        debugLogger.log(logging.DEBUG, str("Found application.xml"))
+
+                        appxml = zipFile.read(nameEntry)
+                        startIndex = appxml.find(startPoint) + len(startPoint)
+                        endIndex = appxml.find(endPoint)
+
+                        debugLogger.log(logging.DEBUG, str(appxml))
+                        debugLogger.log(logging.DEBUG, str(startIndex))
+                        debugLogger.log(logging.DEBUG, str(endIndex))
+
+                        appDisplayName = appxml[startIndex:endIndex]
+
+                        debugLogger.log(logging.DEBUG, str(appDisplayName))
+                    else:
+                        errorLogger.log(logging.ERROR, str("No application.xml file was found in archive {0}.").format(filePath))
+                        raise Exception(str("No application.xml file was found in archive {0}.").format(filePath))
+                    #endif
+                elif (filePath.endswith(".war")):
+                    if (nameEntry.lower().find(("web.xml")) != -1):
+                        debugLogger.log(logging.DEBUG, str("Found web.xml"))
+
+                        appxml = zipFile.read(nameEntry)
+                        startIndex = appxml.find(startPoint) + len(startPoint)
+                        endIndex = appxml.find(endPoint)
+
+                        debugLogger.log(logging.DEBUG, str(appxml))
+                        debugLogger.log(logging.DEBUG, str(startIndex))
+                        debugLogger.log(logging.DEBUG, str(endIndex))
+
+                        appDisplayName = appxml[startIndex:endIndex]
+
+                        debugLogger.log(logging.DEBUG, str(appDisplayName))
+                    else:
+                        errorLogger.log(logging.ERROR, str("No web.xml file was found in archive {0}.").format(filePath))
+                        raise Exception(str("No web.xml file was found in archive {0}.").format(filePath))
+                    #endif
+                else:
+                    errorLogger.log(logging.ERROR, str("Unsupported archive type: {0}.").format(filePath))
+                    raise Exception(str("Unsupported archive type: {0}.").format(filePath))
                 #endif
             #endfor
         else:
-            print ("An unsupported archive type was provided.")
+            errorLogger.log(logging.ERROR, str("Unsupported archive: {0}.").format(filePath))
+            raise Exception(str("Unsupported archive: {0}.").format(filePath))
         #endif
     #endif
+
+    debugLogger.log(logging.DEBUG, str("EXIT: includes#getAppDisplayName(filePath)"))
 
     return appDisplayName
 #enddef
 
 def getAppModuleName(filePath):
+    debugLogger.log(logging.DEBUG, str("ENTER: includes#getAppModuleName(filePath)"))
+    debugLogger.log(logging.DEBUG, str(filePath))
+
     if (os.path.exists(filePath)):
-        start_string = "<web-uri>"
-        end_string = "</web-uri>"
+        startPoint = "<web-uri>"
+        endPoint = "</web-uri>"
+
+        debugLogger.log(logging.DEBUG, str(startPoint))
+        debugLogger.log(logging.DEBUG, str(endPoint))
 
         if (filePath.endswith(".ear")):
-            zf = zipfile.ZipFile(filePath, "r")
-            nl = zf.namelist()
+            zipFile = zipfile.ZipFile(filePath, "r")
+            nameListing = zipFile.namelist()
 
-            for name in nl:
-                if (name.lower().find(("application.xml")) != -1):
-                    appxml = zf.read(name)
-                    start_index = appxml.find(start_string) + len(start_string)
-                    end_index = appxml.find(end_string)
+            debugLogger.log(logging.DEBUG, str(zipFile))
+            debugLogger.log(logging.DEBUG, str(nameListing))
 
-                    appModuleName = appxml[start_index:end_index]
+            for nameEntry in (nameListing):
+                if (nameEntry.lower().find(("application.xml")) != -1):
+                    debugLogger.log(logging.DEBUG, str("Found application.xml"))
+
+                    appxml = zipFile.read(nameEntry)
+                    startIndex = appxml.find(startPoint) + len(startPoint)
+                    endIndex = appxml.find(endPoint)
+
+                    debugLogger.log(logging.DEBUG, str(appxml))
+                    debugLogger.log(logging.DEBUG, str(startIndex))
+                    debugLogger.log(logging.DEBUG, str(endIndex))
+
+                    appModuleName = appxml[startIndex:endIndex]
+
+                    debugLogger.log(logging.DEBUG, str(appModuleName))
+                else:
+                    errorLogger.log(logging.ERROR, str("No application.xml file was found in archive {0}.").format(filePath))
+                    raise Exception(str("No application.xml file was found in archive {0}.").format(filePath))
                 #endif
             #endfor
         else:
-            print ("An unsupported archive type was provided.")
+            errorLogger.log(logging.ERROR, str("Unsupported archive: {0}.").format(filePath))
+            raise Exception(str("Unsupported archive: {0}.").format(filePath))
         #endif
     #endif
+
+    debugLogger.log(logging.DEBUG, str("EXIT: includes#getAppModuleName(filePath)"))
 
     return appModuleName
 #enddef
 
 def getAppWarName(filePath):
+    debugLogger.log(logging.DEBUG, str("ENTER: includes#getAppWarName(filePath)"))
+    debugLogger.log(logging.DEBUG, str(filePath))
+
     if (os.path.exists(filePath)):
-        start_string = "<web-uri>"
-        end_string = "</web-uri>"
+        startPoint = "<web-uri>"
+        endPoint = "</web-uri>"
+
+        debugLogger.log(logging.DEBUG, str(startPoint))
+        debugLogger.log(logging.DEBUG, str(endPoint))
 
         if (filePath.endswith(".ear")):
-            zf = zipfile.ZipFile(filePath, "r")
-            nl = zf.namelist()
+            zipFile = zipfile.ZipFile(filePath, "r")
+            nameListing = zipFile.namelist()
 
-            for name in nl:
+            debugLogger.log(logging.DEBUG, str(zipFile))
+            debugLogger.log(logging.DEBUG, str(nameListing))
+
+            for nameEntry in (nameListing):
                 if (name.lower().endswith(("war")) != -1):
-                    appWarName = name.lower()
+                    debugLogger.log(logging.DEBUG, str("Found war file {0}").format(nameEntry))
+
+                    appWarName = nameEntry.lower()
+
+                    debugLogger.log(logging.DEBUG, str(appWarName))
+                else:
+                    errorLogger.log(logging.ERROR, str("No WAR files were found in archive {0}.").format(filePath))
+                    raise Exception(str("No WAR files were found in archive {0}.").format(filePath))
                 #endif
             #endfor
         else:
-            print ("An unsupported archive type was provided.")
+            errorLogger.log(logging.ERROR, str("Unsupported archive: {0}.").format(filePath))
+            raise Exception(str("Unsupported archive: {0}.").format(filePath))
         #endif
     #endif
+
+    debugLogger.log(logging.DEBUG, str("EXIT: includes#getAppWarName(filePath)"))
 
     return appWarName
 #enddef
 
 def getFileNameFromPath(filePath):
-    return os.path.basename(filePath).split('/')[-1]
-#enddef
+    debugLogger.log(logging.DEBUG, str("ENTER: includes#getFileNameFromPath(appTargetMapping, status)"))
 
-def hasExtraFileExtension(inString):
-    if (not isinstance(inString, str)):
-        raise TypeError("Input must be a string.")
+    fileName = ""
+
+    debugLogger.log(logging.DEBUG, str(fileName))
+
+    if (not isinstance(filePath, str)):
+        fileName = os.path.basename(filePath).split("/")[-1]
     else:
-        return bool(inString.endswith(".war.war"))
+        errorLogger.log(logging.ERROR, str("Unsupported input type: {0}.").format(filePath))
+        raise Exception(str("Unsupported input type: {0}.").format(filePath))
     #endif
+
+    debugLogger.log(logging.DEBUG, str(fileName))
+    debugLogger.log(logging.DEBUG, str("EXIT: includes#getFileNameFromPath(appTargetMapping, status)"))
+
+    return fileName
 #enddef
 
-def removeExtraExtension(inString, removeNumber):
-    if (not isinstance(inString, str)):
-        raise TypeError("Input must be a string.")
-    elif (not isinstance(removeNumber, int)):
-        raise TypeError("Number of characters to remove must be an integer.")
-    elif (removeNumber <= 0):
-        raise ValueError("Number of characters to remove cannot be negative or 0.")
+def hasExtraFileExtension(fileName, extension):
+    debugLogger.log(logging.DEBUG, str("ENTER: includes#hasExtraFileExtension(fileName, extension)"))
+
+    hasExtraFileExtension = bool(False)
+
+    debugLogger.log(logging.DEBUG, str(hasExtraFileExtension))
+
+    if (not isinstance(fileName, str)):
+        hasExtraFileExtension = bool(fileName.endswith(str("{0}.{0}").format(extension, extension)))
+
+        debugLogger.log(logging.DEBUG, str(hasExtraFileExtension))
     else:
-        if (hasExtraFileExtension(inString)):
-            return inString[:-removeNumber]
+        errorLogger.log(logging.ERROR, str("Unsupported input type: {0}.").format(fileName))
+        raise Exception(str("Unsupported input type: {0}.").format(fileName))
+    #endif
+
+    debugLogger.log(logging.DEBUG, str(fileName))
+    debugLogger.log(logging.DEBUG, str("EXIT: includes#hasExtraFileExtension(fileName, extension)"))
+
+    return hasExtraFileExtension
+#enddef
+
+def removeExtraExtension(fileName, removeNumber):
+    debugLogger.log(logging.DEBUG, str("ENTER: includes#removeExtraExtension(fileName, removeNumber)"))
+
+    responseString = ""
+
+    debugLogger.log(logging.DEBUG, str(responseString))
+
+    if ((not isinstance(fileName, str)) and (not isinstance(removeNumber, int))):
+        errorLogger.log(logging.ERROR, str("Unsupported input types: {0} {1}.").format(fileName, removeNumber))
+        raise Exception(str("Unsupported input type: {0} {1}.").format(fileName, removeNumber))
+    else:
+        if (removeNumber <= 0):
+            errorLogger.log(logging.ERROR, str("Characters to remove cannot be equal to or less than 0: {0}.").format(removeNumber))
+            raise Exception(str("Characters to remove cannot be equal to or less than 0: {0}.").format(removeNumber))
         else:
-            return inString
+            if (hasExtraFileExtension(inString)):
+                responseString = fileName[:-removeNumber]
+            #endif
         #endif
     #endif
+
+    debugLogger.log(logging.DEBUG, str(fileName))
+    debugLogger.log(logging.DEBUG, str("EXIT: includes#removeExtraExtension(fileName, removeNumber)"))
+
+    return responseString
 #enddef
 
--- AppServer/wsadmin/includes/readPropertyFile.py
+++ AppServer/wsadmin/includes/readPropertyFile.py
@@ -19,17 +19,32 @@
 import logging
 import ConfigParser
 
+errorLogger = logging.getLogger("error-logger")
 debugLogger = logging.getLogger("debug-logger")
 
 def returnPropertyConfiguration(configFile, sectionName, valueName):
-    debugLogger.log(logging.DEBUG, configFile)
-    debugLogger.log(logging.DEBUG, sectionName)
-    debugLogger.log(logging.DEBUG, valueName)
+    debugLogger.log(logging.DEBUG, str("ENTER: includes#returnPropertyConfiguration(configFile, sectionName, valueName)"))
+    debugLogger.log(logging.DEBUG, str(configFile))
+    debugLogger.log(logging.DEBUG, str(sectionName))
+    debugLogger.log(logging.DEBUG, str(valueName))
+
+    configResponse = ""
+
+    debugLogger.log(logging.DEBUG, str(configResponse))
 
     config = ConfigParser.ConfigParser()
     config.read(configFile)
 
-    debugLogger.log(logging.DEBUG, config)
+    debugLogger.log(logging.DEBUG, str(config))
 
-    return config.get(sectionName, valueName)
+    if (len(config) != 0):
+        configResponse = config.get(str(sectionName), str(valueName))
+    else:
+        errorLogger.log(logging.ERROR, str("Unable to locate value {0}, section {1} in file {3}").format(valueName, sectionName, configFile))
+        raise Exception(str("Unable to locate value {0}, section {1} in file {3}").format(valueName, sectionName, configFile))
+    #endif
+
+    debugLogger.log(logging.DEBUG, str("EXIT: includes#returnPropertyConfiguration(configFile, sectionName, valueName)"))
+
+    return configResponse
 #enddef
