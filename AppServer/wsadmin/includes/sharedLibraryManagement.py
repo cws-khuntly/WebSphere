fed2a1158343aa692aadd65f4af82697722f4017
Kevin Huntly <kmhuntly@gmail.com>
2025-03-12 06:56:18 -0400

    add shared lib stuff

--- AppServer/wsadmin/includes/sharedLibraryManagement.py
+++ AppServer/wsadmin/includes/sharedLibraryManagement.py
@@ -32,8 +32,8 @@ def createSharedLibrary(targetCell, targetLibraryName, targetLibraryClassPath):
 
     if ((len(targetLibraryName) != 0) and (len(targetLibraryClassPath) != 0)):
         try:
-            debugLogger.log(logging.DEBUG, str("Calling AdminConfig.create()"))
-            debugLogger.log(logging.DEBUG, str("EXEC: AdminConfig.create(\"Library\", targetCell, str(\"[[\"name\"', \"{0}\"], [\"classPath\", \"{1}\"]]\").format(targetLibraryName, targetLibraryClassPath)"))
+            debugLogger.log(logging.DEBUG, "Calling AdminConfig.create()")
+            debugLogger.log(logging.DEBUG, "EXEC: AdminConfig.create(\"Library\", targetCell, str(\"[[\"name\"', \"{0}\"], [\"classPath\", \"{1}\"]]\").format(targetLibraryName, targetLibraryClassPath)")
 
             AdminConfig.create("Library", targetCell, str("[[\"name\"', \"{0}\"], [\"classPath\", \"{1}\"]]").format(targetLibraryName, targetLibraryClassPath))
 
@@ -46,23 +46,53 @@ def createSharedLibrary(targetCell, targetLibraryName, targetLibraryClassPath):
             raise Exception(str("An error occurred creating shared library {0}: {1} {2}").format(targetLibraryName, str(exception), str(parms)))
         #endtry
     else:
-        errorLogger.log(logging.ERROR, str("No library name was provided or no classpath was provided."))
+        errorLogger.log(logging.ERROR, "No library name was provided or no classpath was provided.")
 
-        raise Exception(str("No library name was provided or no classpath was provided."))
+        raise Exception("No library name was provided or no classpath was provided.")
     #endif
 
-    debugLogger.log(logging.DEBUG, str("EXIT: sharedLibraryManagement#createSharedLibrary(targetCell, targetLibrary, targetLibraryClassPath)"))
+    debugLogger.log(logging.DEBUG, "EXIT: sharedLibraryManagement#createSharedLibrary(targetCell, targetLibrary, targetLibraryClassPath)")
 #enddef
 
 def addSharedLibraryToServer(targetServer, targetLibraryName):
-    """Creates a classloader on the specified appserver and associates it with a shared library"""
-    m = "createSharedLibraryClassloader:"
-    #sop(m,"Entry. Create shared library classloader. nodename=%s servername=%s libname=%s " % ( repr(nodename), repr(servername), repr(libname) ))
-    server_id = getServerByNodeAndName(nodename, servername )
-    #sop(m,"server_id=%s " % ( repr(server_id), ))
-    appserver = AdminConfig.list('ApplicationServer', server_id)
-    #sop(m,"appserver=%s " % ( repr(appserver), ))
-    classloader = AdminConfig.create('Classloader', appserver, [['mode', 'PARENT_FIRST']])
-    #sop(m,"classloader=%s " % ( repr(classloader), ))
-    result = AdminConfig.create('LibraryRef', classloader, [['libraryName', libname], ['sharedClassloader', 'true']])
-    #sop(m,"Exit. result=%s" % ( repr(result), ))
+    debugLogger.log(logging.DEBUG, "ENTER: sharedLibraryManagement#addSharedLibraryToServer(targetServer, targetLibraryName)")
+    debugLogger.log(logging.DEBUG, targetServer)
+    debugLogger.log(logging.DEBUG, targetLibraryName)
+
+    if ((len(targetServer) != 0) and (len(targetLibraryName) != 0)):
+        try:
+            existingClassLoader = AdminConfig.list("ClassLoader", targetServer)
+
+            debugLogger.log(logging.DEBUG, existingClassLoader)
+
+            if (len(existingClassLoader) == 0):
+                debugLogger.log(logging.DEBUG, "No existing classloader definition found. Creating a new classloader.")
+                debugLogger.log(logging.DEBUG, "Calling AdminConfig.create()")
+                debugLogger.log(logging.DEBUG, "EXEC: AdminConfig.create(\"Classloader\", targetServer, \"[[mode PARENT_LAST]]\")")
+
+                existingClassLoader = AdminConfig.create("Classloader", targetServer, "[[mode PARENT_LAST]]")
+
+                debugLogger.log(logging.DEBUG, existingClassLoader)
+            #endif
+
+            debugLogger.log(logging.DEBUG, "Calling AdminConfig.create()")
+            debugLogger.log(logging.DEBUG, "EXEC: AdminConfig.create(\"LibraryRef\", existingClassLoader, \"[[libraryName \"{0}\"]]\")")
+
+            AdminConfig.create("LibraryRef", existingClassLoader, str("[[libraryName \"{0}\"], [sharedClassloader \"true\"]]").format(targetLibraryName))
+
+            infoLogger.log(logging.INFO, str("Completed adding shared library {0} to server {1}.").format(targetLibraryName, targetServer))
+        except:
+            (exception, parms, tback) = sys.exc_info()
+
+            errorLogger.log(logging.ERROR, str("An error occurred adding shared library {0} to server {1}: {2} {3}").format(targetLibraryName, targetServer, str(exception), str(parms)))
+
+            raise Exception(str("An error occurred adding shared library {0} to server {1}: {2} {3}").format(targetLibraryName, targetServer, str(exception), str(parms)))
+        #endtry
+    else:
+        errorLogger.log(logging.ERROR, str("No library name was provided or no server was provided."))
+
+        raise Exception(str("No library name was provided or no server was provided."))
+    #endif
+
+    debugLogger.log(logging.DEBUG, "EXIT: sharedLibraryManagement#addSharedLibraryToServer(targetServer, targetLibraryName)")
+#enddef
